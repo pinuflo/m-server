c0c04d3db89d06b8c87aa1c39a8d20a8c8ce5b5e
Francisco Mendoza <francisco.mendoza@kibernum.com>
2018-08-07 15:02:13 -0400

    cambios

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
-var app = require('./app');
-var port = process.env.PORT || 3000;
+const express = require('express');
+const logger = require('./libs/logger/app-logger');
+const morgan = require('morgan');
+const config = require('./libs/config/config.dev');
+const db = require('./db');
 
-var server = app.listen(port, function() {
-  console.log('Express server listening on port ' + port);
+const app = express();
+
+const port = config.serverPort;
+
+logger.stream = {
+  write(message, encoding) {
+      logger.info(message);
+  },
+};
+
+app.use(morgan('dev', { stream: logger.stream }));
+
+//Index route
+app.get('/', (req, res) => {
+    res.send('Service');
 });
+
+//AUTH api
+var AuthController = require('./auth/AuthController');
+app.use('/api/auth', AuthController);
+
+app.listen(port, function() {
+    console.log('Server corriendo en puerto  ', port);
+    logger.info('Servidor corriendo - ', port);
+});
+
